e14bb240b2519a9706cdf4c636813e0d134f4de2
Hyun-Kyung Yi <hyun.yi@nb-hyi.xing.hh>
2017-12-06 21:38:32 +0100

    set structure for Rezensionen

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="<?php bloginfo('description'); ?>">
   <meta name="author" content="">
-  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
   <?php wp_head(); ?>
 </head>
 <body>
@@ -22,10 +22,10 @@
   <div class="blog"><a href="<?php echo esc_url( get_permalink( get_page_by_title( 'blog' ) ) );?>">Blog</a></div>
   <div class="portrait-foto">Porträtfoto</div>
   <div class="random-foto">Foto</div>
+    <!-- Blog ist eine eigene Seite -->
   <div class="rezensionen"><a href="<?php echo esc_url( get_permalink( get_page_by_title( 'rezensionen' ) ) );?>">Rezensionen</a></div>
   <div class="lesungen">Lesungen</div>
   <div class="metrofolklore">Metrofolklore</div>
   <div class="lektorate">Privatlektorate</div>
   <div class="stoebern">Stöbern</div>
-  <!-- Zitategenerator sollte  -->
 </div>
--- page-presse.php
+++ page-presse.php
@@ -5,8 +5,17 @@ Template Name: Presse
 
 get_header();
 
+query_posts(array('category_name' => 'rezension'));
+if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  <div class="rezension-post">
+    <a href="<?php echo home_url(); ?>">Back</a>
+    <h2><?php the_title(); ?></h2>
+    <p><?php the_content(); ?></p>
+  </div>
+<?php endwhile; endif; ?>
+<?php wp_reset_query(); ?>
 
-// Next task is to create a custom post_type for REZENSIONEN, maybe check out this link: http://www.wpbeginner.com/wp-tutorials/how-to-create-custom-post-types-in-wordpress/
-// It would also be good to find out how to save blank lines ans spaces inside of posts: http://brentgrossman.com/291/stop-wordpress-from-removing-html/
-
-get_footer(); ?>
+<!-- Next task is to create a custom post_type for REZENSIONEN, maybe check out this link: http://www.wpbeginner.com/wp-tutorials/how-to-create-custom-post-types-in-wordpress/
+It would also be good to find out how to save blank lines ans spaces inside of posts: http://brentgrossman.com/291/stop-wordpress-from-removing-html/
+ -->
+<?php get_footer(); ?>
