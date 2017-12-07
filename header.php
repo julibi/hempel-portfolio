d9cd8576f2bdc6ea56478462f6a56656583ca6e6
Hyun-Kyung Yi <hyun.yi@nb-hyi.xing.hh>
2017-12-07 23:42:35 +0100

    started styling header

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
   <meta name="description" content="<?php bloginfo('description'); ?>">
   <meta name="author" content="">
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <?php wp_head(); ?>
 </head>
 <body>
@@ -17,11 +18,11 @@
   <!-- Pressestimmen unter Metrofolklore ist eine eigenen Seite -->
 
   <div class="bio">Patricia Hempel</div>
-  <div class="fb">Facebook</div>
+  <div class="fb"><i class="fa">&#xf09a;</i></div>
   <!-- Blog ist eine eigene Seite -->
   <div class="blog"><a href="<?php echo esc_url( get_permalink( get_page_by_title( 'blog' ) ) );?>">Blog</a></div>
-  <div class="portrait-foto">Porträtfoto</div>
-  <div class="random-foto">Foto</div>
+  <div class="portrait-foto"></div>
+  <div class="random-foto"></div>
     <!-- Blog ist eine eigene Seite -->
   <div class="rezensionen"><a href="<?php echo esc_url( get_permalink( get_page_by_title( 'rezensionen' ) ) );?>">Rezensionen</a></div>
   <div class="lesungen">Lesungen</div>
